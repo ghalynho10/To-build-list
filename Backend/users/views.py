15710149a9cf1e5af0619478a3f0b0317fc151d1
ghalynho10 <mghalynho@gmail.com>
2020-04-29 13:38:16 -0400

    Adding permissions to users

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import User
 from .serializers import UserSerializer
+from . import permissions
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -9,3 +11,4 @@ from .serializers import UserSerializer
 class UserViewsets(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = (permissions.UpdateOwnProfile, IsAuthenticated,)
