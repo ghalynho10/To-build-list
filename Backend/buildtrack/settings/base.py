9f17857dd8086ccd86a26ac180e8d184e8a8d270
ghalynho10 <mghalynho@gmail.com>
2020-04-28 20:00:34 -0400

    Adding a signal to create user profile

--- Backend/buildtrack/settings/base.py
+++ Backend/buildtrack/settings/base.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     # 'rest_framework.authtoken',
 
     'core',
-    'users',
+    'users.apps.UsersConfig',
 
 ]
 
--- Backend/users/models.py
+++ Backend/users/models.py
@@ -53,18 +53,18 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=140)
-    avatar = models.ImageField(upload_to='avatars/')
+    # avatar = models.ImageField(upload_to='avatars/')
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
 
-        img = Image.open(self.avatar.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.avatar.path)
+    #     img = Image.open(self.avatar.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.avatar.path)
