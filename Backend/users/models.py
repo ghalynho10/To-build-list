2568b020816993a8f5ffad9eefb6b6a71735ea53
ghalynho10 <mghalynho@gmail.com>
2020-04-28 20:11:09 -0400

    Fixing 'is_staff' issue and register Profile model in admin

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -32,6 +32,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     objects = UserManager()
