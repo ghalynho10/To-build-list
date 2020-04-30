3b9cc251dc5a183116611aac03455d99a6a227a7
ghalynho10 <mghalynho@gmail.com>
2020-04-30 19:16:51 -0400

    Implementation of the register page

--- Frontend/src/containers/Register.js
+++ Frontend/src/containers/Register.js
@@ -1,28 +1,90 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
-import * as actions from "../../actions/auth";
+import * as actions from '../store/actions/auth'
+import { Link } from 'react-router-dom'
 
 
 export class Register extends Component {
 
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        password2: ""
+    };
 
     render() {
-        return (
-            <div>
 
+        const { username, email, password, password2 } = this.state;
+        return (
+            <div className="col-md-6 m-auto">
+                <div className="card card-body mt-5">
+                    <h2 className="text-center">Register</h2>
+                    <form onSubmit={this.onSubmit}>
+                        <div className="form-group">
+                            <label>username</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                name="username"
+                                onChange={this.onChange}
+                                value={username}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Email address</label>
+                            <input
+                                type="email"
+                                className="form-control"
+                                name="email"
+                                onChange={this.onChange}
+                                value={email}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                name="password"
+                                onChange={this.onChange}
+                                value={password}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Confirm Password</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                name="password2"
+                                onChange={this.onChange}
+                                value={password2}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-primary">
+                                Register
+              </button>
+                        </div>
+                        <p>
+                            Already have an account? <Link to="/login">Login</Link>
+                        </p>
+                    </form>
+                </div>
             </div>
-        )
+        );
     }
 
 }
 
 const mapStateToProps = (state) => ({
-    loading: state.auth.loading
+    loading: state.auth.loading,
+    isAuthenticated: state.auth.token !== null
 })
 
 const mapDispatchToProps = dispatch => {
     return {
-        onSignup: (username, email, password1, password2, firstname, lastname) => dispatch(username, email, password1, password2, firstname, lastname)
+        onSignup: (username, email, password1, password2, firstname, lastname) => dispatch(actions.authSignup(username, email, password1, password2, firstname, lastname))
     }
 }
 
--- Frontend/src/routes/baseroutes.js
+++ Frontend/src/routes/baseroutes.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { Switch, Route } from "react-router-dom";
 import Login from '../containers/Login'
+import Register from '../containers/Register'
 
 export class baseroutes extends Component {
     render() {
         return (
             <Switch>
                 <Route exact path='/login' component={Login} />
+                <Route exact path='/register' component={Register} />
             </Switch>
         )
     }
